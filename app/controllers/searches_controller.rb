8e5215b07af4867b0affaaa410fe7ff93763918c
HidekiShiba <hshiba033@gmail.com>
2020-12-19 19:40:56 +0900

    [Add] 検索ワード表示

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,8 +3,10 @@ class SearchesController < ApplicationController
     @range = params[:range]
 
     if @range == "User"
+      @word = params[:word]
       @users = User.looks(params[:search], params[:word])
     else
+      @word = params[:word]
       @books = Book.looks(params[:search], params[:word])
     end
   end
